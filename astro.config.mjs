3107cdd6517f1b7a76711e369643693a5b9dc8d6
SchoolyB <marshallbcodes@gmail.com>
2025-11-30 23:43:07 -0600

    Fix base URL trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://schoolyb.github.io',
-  base: '/language.ez',
+  base: '/language.ez/',
   vite: {
     plugins: [tailwindcss()]
   }
